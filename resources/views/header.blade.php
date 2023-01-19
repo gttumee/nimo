8612b3394d5c0e5eb5e5c1b635de0320379d6c21
tumee <tmkee0525@gmail.com>
2023-01-19 23:46:28 +0900

    error page change

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -28,13 +28,15 @@
       <!-- header section strats -->
       <header class="header_section">
         <div class="container">
-          <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <nav class="navbar navbar-expand-lg custom_nav-container">
+            <a href="{{route('index')}}">
             <img src="/images/logo.png" class="logo" alt="">
             <a class="navbar-brand" href="{{route('index')}}">
               <span>
                 Нимо Япон хэл            
               </span> 
             </a>
+          </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
